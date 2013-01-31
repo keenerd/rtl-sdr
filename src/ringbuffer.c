f2ef59605172f980e927ce647d1a6ac03d49dd95
Elias Oenal <EliasOenal@gmail.com>
2013-01-31 04:44:01 +0100

    lockfree inter-thread communication

---
+++ src/ringbuffer.c
@@ -0,0 +1,34 @@
+// See header for information
+#include <ringbuffer.h>
+#include <string.h>
+
+void ringbuffer_init(ringbuffer *rb, int total_size, int element_size)
+{
+    rb->total_size = total_size;
+    rb->element_size = element_size;
+    rb->head = 0;
+    rb->tail = 0;
+}
+
+int ringbuffer_is_full(volatile const ringbuffer *rb)
+{
+    return (rb->tail + rb->element_size) % rb->total_size == rb->head;
+}
+
+int ringbuffer_is_empty(volatile const ringbuffer *rb)
+{
+    return rb->tail == rb->head;
+}
+
+void ringbuffer_write(volatile ringbuffer *rb, const unsigned char *element)
+{
+    memcpy((void*)&rb->elems[rb->tail], (void*)element, rb->element_size);
+    rb->tail = (rb->tail + rb->element_size) % rb->total_size;
+    if(ringbuffer_is_empty(rb)) rb->head = (rb->head + 1) % rb->total_size;
+}
+
+void ringbuffer_read(volatile ringbuffer *rb, unsigned char *element)
+{
+    memcpy((void*)element, (void*)&rb->elems[rb->head], rb->element_size);
+    rb->head = (rb->head + rb->element_size) % rb->total_size;
+}
