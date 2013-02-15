6c33b45a534a956d48890a716e15725134b9b259
Elias Oenal <EliasOenal@gmail.com>
2013-02-15 08:24:19 +0100

    fence instructions

--- src/ringbuffer.c
+++ src/ringbuffer.c
@@ -23,6 +23,7 @@ int ringbuffer_is_empty(volatile const ringbuffer *rb)
 void ringbuffer_write(volatile ringbuffer *rb, const unsigned char *element)
 {
     memcpy((void*)&rb->elems[rb->tail], (void*)element, rb->element_size);
+    __sync_synchronize();
     rb->tail = (rb->tail + rb->element_size) % rb->total_size;
     if(ringbuffer_is_empty(rb)) rb->head = (rb->head + 1) % rb->total_size;
 }
@@ -30,5 +31,6 @@ void ringbuffer_write(volatile ringbuffer *rb, const unsigned char *element)
 void ringbuffer_read(volatile ringbuffer *rb, unsigned char *element)
 {
     memcpy((void*)element, (void*)&rb->elems[rb->head], rb->element_size);
+    __sync_synchronize();
     rb->head = (rb->head + rb->element_size) % rb->total_size;
 }
